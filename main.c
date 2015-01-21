14aee6f9ded2034dca98188ef0c1ad94e66b2f23
pierre-ignas <pierre.ignas@bts-malraux.net>
2015-01-21 12:03:12 +0100

    premier truc

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+Double  resultat* = 0, resultat/ = 0, operandeA= 0, operandeB = 0;
+
+cout <<"Saisissez une valeur pour A : "<< endl;
+cin >> operandeA
+
+cout <<"Saisissez une valeur pour B : "<< endl;
+cin >> operandeB
+
+cout <<"
+} 
